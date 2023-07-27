f82c4c5d8286dabbfb598c8a8b8e3a0beb487b95
amankakkar2002 <97812276+amankakkar2002@users.noreply.github.com>
2023-07-27 12:57:20 +0530

    Update Available.js

--- src/components2/Available.js
+++ src/components2/Available.js
@@ -6,28 +6,18 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Swapped = () => {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   const getAllUser = () => {
     fetch("/AllUser", {
       method: "GET",
     })
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error('Failed to fetch data from the server');
-        }
-        return res.json();
-      })
+      .then((res) => res.json())
       .then((data) => {
         console.log(data, "userData");
         setData(data.data);
-        setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
-        setError('Failed to fetch data. Please try again later.');
-        setLoading(false);
       });
   };
 
@@ -50,11 +40,7 @@ const Swapped = () => {
         <h6> </h6>
 
         <div className='col-md-6'>
-          {loading ? (
-            <p>Loading...</p>
-          ) : error ? (
-            <p>{error}</p>
-          ) : data.length > 0 ? (
+          {data.length > 0 ? (
             <table style={{ width: 1100 }}>
               <tr>
                 <td style={{ border: '1px solid #ddd', padding: '8px' }}><b>Name</b></td>
