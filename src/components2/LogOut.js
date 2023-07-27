18f8d35936b6af5e2889c2a58f44b37fd00f8490
amankakkar2002 <97812276+amankakkar2002@users.noreply.github.com>
2023-07-27 13:49:55 +0530

    Update LogOut.js

--- src/components2/LogOut.js
+++ src/components2/LogOut.js
@@ -3,35 +3,34 @@ import { useNavigate } from "react-router-dom";
 
 const LogOut = () => {
     const navigate = useNavigate();
-    useEffect(()=>
-    {
-        fetch('/logout',
-        {
-            method:'GET',
-            headers:
-            {
-                Accept:'application/json',
-                "Content-Type":"application/json"
+    useEffect(() => {
+        fetch('https://swap-ease-backend.vercel.app/logout', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                "Content-Type": "application/json"
             },
-            credentials:'include'
-        }).then((res)=>
-            {
-                navigate('/login',{replace:true});
+            credentials: 'include'
+        })
+            .then((res) => {
+                navigate('/login', { replace: true });
                 if (!res.status === 200) {
                     const error = new Error(res.error);
                     throw error;
-                  }
-            }).catch((err)=>
-            {
+                } else {
+                    localStorage.removeItem("userToken"); // Remove the token from local storage after successful logout
+                }
+            })
+            .catch((err) => {
                 console.log(err);
             })
-    })
+    }, [navigate]);
 
-  return (
-    <>
-      <h4 align='center' className='demo2'> Logging out....</h4>
-    </>
-  )
+    return (
+        <>
+            <h4 align='center' className='demo2'> Logging out....</h4>
+        </>
+    )
 }
 
-export default LogOut
+export default LogOut;
